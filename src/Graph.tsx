f36afab9dc881616d6dce71b166390871ca4a24e
Bhavya Singh <hriddhu7@gmail.com>
2023-01-10 17:17:53 +0530

    changes for graph to make is visual

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -53,12 +53,12 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute("column-pivots", '["stock"]');
       elem.setAttribute("row-pivots", '["timestamp"]');
       elem.setAttribute("columns", '["top_ask_price"]');
-      elem.setAttribute("aggregates",`
-      { "stock": "distinct_count",
-        "top_ask_price": "avg",
-        "top_bid_price": "avg",
-        "timestamp": "distinct_count",}`
-        );
+      elem.setAttribute('aggregates', JSON.stringify({
+        stock: 'distinctcount',
+        top_ask_price: 'avg',
+        top_bid_price: 'avg',
+        timestamp: 'distinct count',
+      }));
     }
   }
 
